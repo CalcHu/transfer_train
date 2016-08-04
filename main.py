ac8b85ae78e5e29f27e37ed9c68ffdf4db477d0d
huhao <huhao@playcrab.com>
2016-08-04 17:49:24 +0800

    支持火车站名 填写全拼和首字母缩写

--- main.py
+++ main.py
@@ -5,19 +5,18 @@ from prettytable import PrettyTable
 
 
 def query():
-    start_station = input('请输入出发站名: ')
-    end_station = input('请输入目的地站名：')
+    start_station = input('请输入出发站名: ').lower()
+    start_code = get_station_code(start_station)
+    end_code = get_station_code(input('请输入目的地站名：').lower())
     is_transfer = input('是否需要中转：0，不需要；1，需要:    ')
     if is_transfer in ['1']:
-        transfer_station = input('请输入中转站名：')
+        transfer_code = get_station_code(input('请输入中转站名：').lower())
     else:
-        transfer_station = None
+        transfer_code = None
 
-    station_dict = get_station_dict()
-    check_station_available(station_dict, start_station, end_station, transfer_station)
     if is_transfer == '1':
-        railway_info1 = get_railway_info(station_dict[start_station], station_dict[transfer_station])['data']['datas']
-        railway_info2 = get_railway_info(station_dict[transfer_station], station_dict[end_station])['data']['datas']
+        railway_info1 = get_railway_info(start_code, transfer_code)['data']['datas']
+        railway_info2 = get_railway_info(transfer_code, end_code)['data']['datas']
 
         arrive_time1 = {}
         start_time2 = {}
@@ -57,7 +56,7 @@ def query():
                     ])
 
     if is_transfer == '0':
-        railway_info = get_railway_info(station_dict[start_station], station_dict[end_station])['data']['datas']
+        railway_info = get_railway_info(start_code, end_code)['data']['datas']
         table_data = PrettyTable(['始发站', '出发时间', '车次', '终到时间', '终点站'])
 
         for i in railway_info:
@@ -66,19 +65,47 @@ def query():
     print(table_data)
 
 
-def check_station_available(station_dict, *args):
-    for i in args:
-        if i is not None and i not in station_dict:
-            print(i + 'is not a legal station name')
-            exit()
+def get_station_code(station):
+    if station in station_cn_name_dict:
+        # 中文站名
+        station_cn_name = station
+    elif station in station_full_pinyin:
+        # 站名全拼
+        length = len(station_full_pinyin[station])
+        if length > 1:
+            _index = 1
+            input_str = "请选择站名:\n"
+            while(_index <= length):
+                input_str += "{}:{}\n".format(_index, station_full_pinyin[station][_index-1])
+                _index = _index + 1
+            ans = int(input(input_str)) - 1
+            station_cn_name = station_full_pinyin[station][ans]
+        else:
+            station_cn_name = station_full_pinyin[station][0]
+
+    else:
+        # 站名简拼
+        length = len(station_simple_pinyin[station])
+        if length > 1:
+            _index = 1
+            input_str = "请选择站名:\n"
+            while(_index <= length):
+                input_str += "{}:{}\n".format(_index, station_simple_pinyin[station][_index-1])
+                _index = _index + 1
+            ans = int(input(input_str)) - 1
+            station_cn_name = station_simple_pinyin[station][ans]
+        else:
+            station_cn_name = station_simple_pinyin[station][0]
+    print ("已选择火车站：{}\n".format(station_cn_name))
+    return station_cn_name_dict[station_cn_name]
 
 
-def get_station_dict():
+def get_station_info_list():
     station_url = config.url_base + '/resources/js/framework/station_name.js'
     station_vars = connection.request_server(station_url, 'get', 'text')
     station_str = station_vars[21:-2]
     _list = station_str.split('@')
-    return dict(i.split('|')[1:3] for i in _list)
+    return _list
 
 
 def get_railway_info(start_station, end_station):
@@ -87,4 +114,19 @@ def get_railway_info(start_station, end_station):
     return connection.request_server(query_uri)
 
 if __name__ == '__main__':
+    station_list = get_station_info_list()
+    station_cn_name_dict = dict(i.split('|')[1:3] for i in station_list)
+    station_full_pinyin = {}
+    station_simple_pinyin = {}
+    for i in station_list:
+        j = i.split('|')
+        if j[3] in station_full_pinyin:
+            station_full_pinyin[j[3]].append(j[1])
+        else:
+            station_full_pinyin[j[3]] = [j[1]]
+
+        if j[4] in station_simple_pinyin:
+            station_simple_pinyin[j[4]].append(j[1])
+        else:
+            station_simple_pinyin[j[4]] = [j[1]]
     query()
